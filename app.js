d7768b79a154003ecd957569bb511fb0c6c0b930
Mehdi Ayache <ayache@kizeo.com>
2019-05-24 17:48:40 +0200

    map is working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + "/src/"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/src/public/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
